55e1a0564e557005d4df821bc352a0b68e9fe6d3
micaela160 <msalas@udesa.edu.ar>
2021-09-11 15:22:49 -0300

    personalizacion fonts avatars foto

--- data/data.js
+++ data/data.js
@@ -5,51 +5,79 @@ let db = {
       imagenPost: "/images/img1.jpeg",
     },
     {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img2.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img3.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img4.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img5.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img6.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img7.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img8.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img9.jpeg",
-      },
-      {
-        descripcion: "Voca A Voca",
-        imagenPost: "/images/img10.jpeg",
-      },
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img2.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img3.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img4.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img5.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img6.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img7.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img8.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img9.jpeg",
+    },
+    {
+      descripcion: "Voca A Voca",
+      imagenPost: "/images/img10.jpeg",
+    },
   ],
-  listaUsers:[
-      {
-          nombreUsuario: 'Mica',
-          seguidores: 8,
-          publicaciones: 2,
-          seguidos: 8,
-          imgPerfil:"/images/Logo.png",
-      }
-  ]
+  listaUsers: [
+    {
+      nombreUsuario: "mariaperez",
+      seguidores: 22,
+      publicaciones: 15,
+      seguidos: 15,
+      imgPerfil: "/images/avatar/avatarF.png",
+    },
+    {
+      nombreUsuario: "santiago_2021",
+      seguidores: 8,
+      publicaciones: 2,
+      seguidos: 8,
+      imgPerfil: "/images/avatar/avatarM.png",
+    },
+    {
+      nombreUsuario: "rodrigoavatar",
+      seguidores: 5,
+      publicaciones: 18,
+      seguidos: 6,
+      imgPerfil: "/images/avatar/avatarM.png",
+    },
+    {
+      nombreUsuario: "micaela19",
+      seguidores: 8,
+      publicaciones: 2,
+      seguidos: 8,
+      imgPerfil: "/images/avatar/avatarF.png",
+    },
+    {
+      nombreUsuario: "usuarioanonimo14",
+      seguidores: 0,
+      publicaciones: 2,
+      seguidos: 20,
+      imgPerfil: "/images/avatar/avatarM.png",
+    },
+  ],
 };
 
 module.exports = db;
