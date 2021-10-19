5683c4a6b0e2b8f75969a4b60bf4f00d54f3f612
micaela160 <msalas@udesa.edu.ar>
2021-10-19 20:28:25 -0300

    asos2

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,15 @@ const db = require('../data/postData');
 let models = require('../database/models')
 let mainController = {
     index: function(req, res, next) {
-        models.Post.findAll()
+        models.Post.findAll({
+            include: [{
+                association: 'user'
+            },
+        {
+            association: 'comments',
+            include:{association: 'user'}
+        }]
+        })
         .then(posteos => {
             res.render('index', { title: 'Voca a Voca', data: posteos });
         }) 
